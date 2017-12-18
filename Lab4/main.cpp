e74af13cedee3b45e27f4660a2548ead73e48a35
WangChen0902 <827913668@qq.com>
2017-12-18 22:41:36 +0800

    最终版

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <fstream>
-#include <map>
 #include <string>
 
 using namespace std;
 
 const string SRC = "D:\\WCs\\Lab4\\Lab4\\input.txt";
+const string SAVE = "D:\\WCs\\Lab4\\Lab4\\output.txt";
 
+//存放DNA序列和其逆序数的类
+class DNA_Struct {
+public:
+	int inv;
+	string content;
+
+	DNA_Struct() {
+		this->content = "";
+		this->inv = 0;
+	}
+
+	DNA_Struct(string content, int inv) {
+		this->content = content;
+		this->inv = inv;
+	}
+
+	void setValue(string content, int inv) {
+		this->content = content;
+		this->inv = inv;
+	}
+
+};
+
+//获得DNA序列的逆序数
 int getInv(string str, int n) {
 	int inv = 0;
 	for (int i = 0; i < n; i++) {
@@ -21,19 +45,40 @@ int getInv(string str, int n) {
 
 int main()
 {
+	//读取并存储数据
 	ifstream inf(SRC);
 	int n, m;
 	inf >> n >> m;
-	string DNA[128];
+	DNA_Struct DNA[8];
 	for (int i = 0; i < m; i++) {
-		inf >> DNA[i];
+		string temp;
+		inf >> temp;
+		DNA[i].setValue(temp, getInv(temp,n));
 	}
-	map<string, int> DNA_Map;
+	inf.close();
+	/*for (int j = 0; j < m; j++) {
+		cout << DNA[j].content << " " << DNA[j].inv << endl;
+	}*/
+	cout << endl;
+	DNA_Struct min = DNA_Struct(DNA[0].content, DNA[0].inv);
+	int minIndex = 0;
 	for (int i = 0; i < m; i++) {
-		DNA_Map.insert(pair<string,int>(DNA[i], getInv(DNA[i],n)));
+		min = DNA_Struct(DNA[i].content, DNA[i].inv);
+		minIndex = i;
+		for (int j = i; j < m; j++) {
+			if (DNA[j].inv < DNA[minIndex].inv) {
+				min.setValue(DNA[j].content,DNA[j].inv);
+				minIndex = j;
+			}
+		}
+		DNA_Struct temp = DNA_Struct(min.content,min.inv);
+		DNA[minIndex].setValue(DNA[i].content,DNA[i].inv);
+		DNA[i].setValue(temp.content,temp.inv);
+		//cout << i << " " << DNA[i].content << " " << DNA[i].inv << endl;
 	}
-	for (map<string, int>::iterator it = DNA_Map.begin(); it != DNA_Map.end(); it++) {
-		cout << it->first << " " << it->second << endl;
+	ofstream ofs(SAVE);
+	for (int j = 0; j < m; j++) {
+		ofs << DNA[j].content << endl;
 	}
 	return 0;
 }
